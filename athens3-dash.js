69320f963ce1b20bfb7c228cb9a9d073709dcb5c
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2023-05-27 11:49:10 -0500

    added a few more widgets

--- athens3-dash.js
+++ athens3-dash.js
@@ -3,7 +3,7 @@ function toggleDropdown(dropdownId) {
     dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
   }
 
-
+// Keygen widget
 fetch('http://3.132.197.22:8088/http://3.218.170.198:1317/zeta-chain/crosschain/keygen', {
     method: 'GET',
 }).then(response => {
@@ -22,6 +22,8 @@ fetch('http://3.132.197.22:8088/http://3.218.170.198:1317/zeta-chain/crosschain/
     console.log("fetch error" + error);
 })
 
+
+// Network status widget
 fetch('http://3.132.197.22:8088/http://3.218.170.198:26657/status', {
     method: 'GET',
 }).then(response => {
@@ -42,3 +44,67 @@ fetch('http://3.132.197.22:8088/http://3.218.170.198:26657/status', {
 }).catch(error => {
     console.log("fetch error" + error);
 })
+
+// supply widget
+fetch('http://3.132.197.22:8088/http://3.218.170.198:1317/cosmos/bank/v1beta1/supply', {
+    method: 'GET',
+}).then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}).then(data => {
+    let div = document.getElementById('supply');
+    div.textContent = JSON.stringify(data, null, 2);
+
+    let amount = BigInt(data.supply[0].amount);
+    let amountZeta = amount/BigInt(1e18); 
+    let div2 = document.getElementById('supply-summary');
+    let summary = {"supply": addCommas((amountZeta).toString()), "denom": "ZETA"};
+    div2.textContent = JSON.stringify(summary, null, 2);
+}).catch(error => {
+    console.log("fetch error" + error);
+})
+
+// validators
+{
+    var divElement = document.createElement("div");
+    var widgets = document.getElementById("widgets");
+    widgets.appendChild(divElement);
+    var header = document.createElement("b");
+    header.textContent = "Validators";
+    divElement.appendChild(header);
+    var summaryPre = document.createElement("pre");
+    divElement.appendChild(summaryPre);
+    var button = document.createElement("button");
+    button.textContent = "Raw JSON"; 
+    button.onclick = function() {toggleDropdown("validators-dropdown")};
+    divElement.appendChild(button);
+    var detailPre = document.createElement("pre");
+    detailPre.id = "validators-dropdown";
+    divElement.appendChild(detailPre);
+    detailPre.classList = "dropdown-content";
+    fetch('http://3.132.197.22:8088/http://3.218.170.198:1317/cosmos/staking/v1beta1/validators', {
+        method: 'GET',
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    }).then(data => {
+        detailPre.textContent = JSON.stringify(data, null, 2); 
+        let jailed = 0;
+        let jailed_monikers = []; 
+        for (let i=0; i<data.validators.length; i++) {
+            let val = data.validators[i];
+            if (val.jailed) {
+                jailed++;
+                jailed_monikers.push(val.description.moniker);
+            }
+        }
+        var summary = {"num_validators": data.validators.length, "num_jailed": jailed, "jailed_monikers": jailed_monikers};
+        summaryPre.textContent = JSON.stringify(summary, null, 2);
+    }).catch(error => {
+        console.log("fetch error" + error);
+    })
+}
---
+++ utils.js
@@ -0,0 +1,6 @@
+// Function to add commas to a number string
+function addCommas(numberString) {
+    var parts = numberString.split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+  }
