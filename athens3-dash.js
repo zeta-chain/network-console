d949a795389ed7765fa8bae88633c4e9704882ae
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2023-06-05 17:31:00 -0500

    added pending outbound queue based on pendingNonces

--- athens3-dash.js
+++ athens3-dash.js
@@ -219,6 +219,35 @@ upgrade_plan();
     })
 }
 
+async function pendingOutboundQueue() {
+    try {
+	let resource = "zeta-chain/crosschain/TSS";
+	let p1 = await fetch(`${nodeURL}/${resource}`, {
+	    method: 'GET',
+	});
+	let data = await p1.json();
+	let TSS = data.TSS.tss_pubkey;
+	console.log("TSS", TSS);
+
+	resource = "zeta-chain/crosschain/pendingNonces";
+	let p2 = await fetch(`${nodeURL}/${resource}`, { method: 'GET', });
+	let data2 = await p2.json();
+	let pending = data2.pending_nonces;
+	pending = pending.filter( (x) => x.tss == TSS);
+	console.log("PENDING", pending);
+
+	let pre = document.getElementById("pending-outbound-queues");
+	pre.textContent = JSON.stringify(data2, null, 2);
+
+	let div = document.getElementById("pending-outbound-queues-summary");
+	div.appendChild(makeTableElement2(pending, ["chain_id", "nonce_low", "nonce_high"]));
+	
+    } catch (error) {
+	console.log('error', error);
+    }
+}
+pendingOutboundQueue();
+
 
 // foreign coins
 {
