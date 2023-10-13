2aac4de9c3943966c33e48f3eeb26f6c0644ebc4
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2023-10-13 13:49:57 -0700

    reformat spaces

--- wallet.js
+++ wallet.js
@@ -3,7 +3,18 @@ import './ecpair.js';
 import './secp256k1.js';
 import './buffer.js';
 import './web3.min.js';
-import {esploraAPIURL, getForegienCoins, RPCByChainID,evmURL,addDetails,makeTableElement,makeTableElement2,Chains, externalChainIDs, network} from './common.js';
+import {
+    esploraAPIURL,
+    getForegienCoins,
+    RPCByChainID,
+    evmURL,
+    addDetails,
+    makeTableElement,
+    makeTableElement2,
+    Chains,
+    externalChainIDs,
+    network
+} from './common.js';
 import {encodings, decode, convertbits} from './bech32.js';
 
 
@@ -12,47 +23,49 @@ let ZRC20ABI;
 let UNISWAPV2FACTORYABI;
 let UNISWAPV2ROUTER02ABI;
 let UNISWAPV2PAIRABI;
+
 async function read_abis() {
     try {
-	let p3 = await fetch('abi/ZRC20.json');
-	let data3 = await p3.json();
-	ZRC20ABI = data3.abi;
-	console.log("set ZRC20ABI", ZRC20ABI);
+        let p3 = await fetch('abi/ZRC20.json');
+        let data3 = await p3.json();
+        ZRC20ABI = data3.abi;
+        console.log("set ZRC20ABI", ZRC20ABI);
     } catch (err) {
-	console.log(err);
+        console.log(err);
     }
     try {
-	let p3 = await fetch('abi/UniswapV2Factory.json');
-	let data3 = await p3.json();
-	UNISWAPV2FACTORYABI = data3.abi;
-	console.log("set UNISWAPV2FACTORYABI", UNISWAPV2FACTORYABI);
+        let p3 = await fetch('abi/UniswapV2Factory.json');
+        let data3 = await p3.json();
+        UNISWAPV2FACTORYABI = data3.abi;
+        console.log("set UNISWAPV2FACTORYABI", UNISWAPV2FACTORYABI);
     } catch (err) {
-	console.log(err);
+        console.log(err);
     }
     try {
-	let p3 = await fetch('abi/UniswapV2Router02.json');
-	let data3 = await p3.json();
-	UNISWAPV2ROUTER02ABI = data3.abi;
-	console.log("set UNISWAPV2ROUTER02ABI", UNISWAPV2ROUTER02ABI);
+        let p3 = await fetch('abi/UniswapV2Router02.json');
+        let data3 = await p3.json();
+        UNISWAPV2ROUTER02ABI = data3.abi;
+        console.log("set UNISWAPV2ROUTER02ABI", UNISWAPV2ROUTER02ABI);
     } catch (err) {
-	console.log(err);
+        console.log(err);
     }
     try {
-	let p3 = await fetch('abi/UniswapV2Pair.json');
-	let data3 = await p3.json();
-	UNISWAPV2PAIRABI = data3.abi;
-	console.log("set UNISWAPV2PAIRABI", UNISWAPV2PAIRABI);
+        let p3 = await fetch('abi/UniswapV2Pair.json');
+        let data3 = await p3.json();
+        UNISWAPV2PAIRABI = data3.abi;
+        console.log("set UNISWAPV2PAIRABI", UNISWAPV2PAIRABI);
     } catch (err) {
-	console.log(err);
+        console.log(err);
     }
 }
+
 await read_abis();
 
 const ecc = secp256k1;
 const ECPair = ecpair.ECPairFactory(ecc);
 
-let  NETWORK;
-let  BLOCKCYPHER;
+let NETWORK;
+let BLOCKCYPHER;
 if (network == "mockmain") {
     NETWORK = bitcoin.networks.mainnet;
     BLOCKCYPHER = "https://api.blockcypher.com/v1/btc/main";
@@ -62,7 +75,6 @@ if (network == "mockmain") {
 }
 const Buffer = buffer.Buffer;
 const hash160 = (b) => bitcoin.crypto.ripemd160(bitcoin.crypto.sha256(b));
-
 
 
 const validator = (
@@ -78,7 +90,7 @@ const validator = (
 // console.log(decoded); 
 // end test
 
-( () => {
+(() => {
     const inputKey = document.getElementById("input-private-key");
     localStorage.getItem("privateKey") && (inputKey.value = localStorage.getItem("privateKey"));
 })();
@@ -139,7 +151,7 @@ document.getElementById('button-send').addEventListener('click', async () => {
 });
 
 document.getElementById('button-generate-key').addEventListener('click', () => {
-    const keyPair = ECPair.makeRandom({ network: NETWORK });
+    const keyPair = ECPair.makeRandom({network: NETWORK});
     console.log("keyPair", keyPair.privateKey);
     let pre = document.getElementById("output-private-key");
     pre.innerHTML = "Private key (please save it): " + keyPair.privateKey.toString('hex');
@@ -149,9 +161,9 @@ document.getElementById('button-set-key').addEventListener('click', async () => 
     const keyInput = document.getElementById("input-private-key");
     const keyHex = keyInput.value;
     localStorage.setItem("privateKey", keyHex);
-    key = ECPair.fromPrivateKey(Buffer.from(keyHex, 'hex'), { network: NETWORK });
+    key = ECPair.fromPrivateKey(Buffer.from(keyHex, 'hex'), {network: NETWORK});
     const addressPre = document.getElementById("key-info");
-    const {address} = bitcoin.payments.p2wpkh({ pubkey: key.publicKey, network: NETWORK });
+    const {address} = bitcoin.payments.p2wpkh({pubkey: key.publicKey, network: NETWORK});
     p2wpkhAddress = address;
     addressPre.innerHTML = "P2WPKH (SegWit) Address: " + address;
 
@@ -160,7 +172,7 @@ document.getElementById('button-set-key').addEventListener('click', async () => 
     updateUTXO();
     updateAddressInfo();
     updateTxs();
-    updateEthAccount(); 
+    updateEthAccount();
 
     console.log("WIF", key.toWIF());
 
@@ -189,7 +201,7 @@ async function updateAddressInfo() {
     balance.replaceChildren(
         DETAILS(
             SUMMARY(TEXT(`Address Info: ${data.address} - balance ${bal} sats`)),
-			PRE(TEXT(JSON.stringify(data, null, 2)))
+            PRE(TEXT(JSON.stringify(data, null, 2)))
         ));
 }
 
@@ -214,54 +226,54 @@ async function makeTransaction(to, amount, utxos, memo) {
     let sum = 0;
     const pickUtxos = [];
     for (let i = 0; i < utxos.length; i++) {
-	const utxo = utxos[i];
-	sum += utxo.value;
-	pickUtxos.push(utxo);
-	if (sum >= total) break;
+        const utxo = utxos[i];
+        sum += utxo.value;
+        pickUtxos.push(utxo);
+        if (sum >= total) break;
     }
     if (sum < total) throw new Error("Not enough funds");
     const change = sum - total;
     const txs = []; // txs corresponding to the utxos
     for (let i = 0; i < pickUtxos.length; i++) {
-	const utxo = pickUtxos[i];
-	const p1 = await fetch(`${esploraAPIURL}/tx/${utxo.txid}`);
-	const data = await p1.json();
-	txs.push(data);
+        const utxo = pickUtxos[i];
+        const p1 = await fetch(`${esploraAPIURL}/tx/${utxo.txid}`);
+        const data = await p1.json();
+        txs.push(data);
     }
     console.log("pickUtxos", pickUtxos);
     console.log("txs", txs);
-    
+
     // try creating a transaction
     const psbt = new bitcoin.Psbt({network: NETWORK});
     psbt.addOutput({address: to, value: amount});
 
     if (memo.length > 0) {
-	const embed = bitcoin.payments.embed({data: [memo]});
-	psbt.addOutput({script: embed.output, value: 0});
+        const embed = bitcoin.payments.embed({data: [memo]});
+        psbt.addOutput({script: embed.output, value: 0});
     }
     if (change > 0) {
-	psbt.addOutput({address: p2wpkhAddress, value: change});
+        psbt.addOutput({address: p2wpkhAddress, value: change});
     }
 
 
     for (let i = 0; i < pickUtxos.length; i++) {
-	const utxo = pickUtxos[i];
-	const inputData = {};
-	inputData.hash = txs[i].txid;
-	inputData.index = utxo.vout;
-	const witnessUtxo = {script: Buffer.from(txs[i].vout[utxo.vout].scriptpubkey, 'hex'), value: utxo.value};
-	inputData.witnessUtxo = witnessUtxo;
-	psbt.addInput(inputData);
+        const utxo = pickUtxos[i];
+        const inputData = {};
+        inputData.hash = txs[i].txid;
+        inputData.index = utxo.vout;
+        const witnessUtxo = {script: Buffer.from(txs[i].vout[utxo.vout].scriptpubkey, 'hex'), value: utxo.value};
+        inputData.witnessUtxo = witnessUtxo;
+        psbt.addInput(inputData);
 
     }
     for (let i = 0; i < pickUtxos.length; i++) {
-	psbt.signInput(i, key); 
-	if (!psbt.validateSignaturesOfInput(i, validator)) {
-	    throw new Error("invalid input");
-	}
+        psbt.signInput(i, key);
+        if (!psbt.validateSignaturesOfInput(i, validator)) {
+            throw new Error("invalid input");
+        }
     }
 
-    psbt.finalizeAllInputs(); 
+    psbt.finalizeAllInputs();
     console.log(psbt);
     console.log(psbt.extractTransaction().toHex());
     // broadcast txid: 3532fceaf248205cb8ebfcbe56d659187ae509b559003c8a3abf863217691e17
@@ -329,13 +341,13 @@ document.getElementById('button-eth-send').addEventListener('click', async () =>
     // validate address & amount
     const to = document.getElementById("input-eth-recipient").value;
     if (!Web3.utils.isAddress(to)) {
-	alert("Invalid recipient address");
-	return; 
+        alert("Invalid recipient address");
+        return;
     }
     const amount = document.getElementById("input-eth-amount").value;
     if (isNaN(amount)) {
-	alert("Invalid amount");
-	return;
+        alert("Invalid amount");
+        return;
     }
     const gasPrice = await web3zevm.eth.getGasPrice();
     console.log("gasPrice", gasPrice);
@@ -343,20 +355,20 @@ document.getElementById('button-eth-send').addEventListener('click', async () =>
 
     const account = ethAccount;
     let p = await account.signTransaction({
-	to: to,
-	value: Web3.utils.toWei(amount),
-	gas: "21000",
-	gasPrice: "1000000000",
+        to: to,
+        value: Web3.utils.toWei(amount),
+        gas: "21000",
+        gasPrice: "1000000000",
     });
     const div = document.getElementById("eth-transaction-receipt");
     div.appendChild(addDetails(`Signed Transaction  ${p.transactionHash}`, JSON.stringify(p, null, 2)));
     web3zevm.eth.sendSignedTransaction(p.rawTransaction).on('receipt', (x) => {
-	div.appendChild(addDetails(`Transaction Receipt ${x.transactionHash}`, JSON.stringify(x, null, 2)));
-	const a = document.createElement("a");
-	a.href = `https://zetachain-athens-3.blockscout.com/tx/${x.transactionHash}`;
-	a.innerText = "View receipt on Blockscout";
-	a.target = "_blank";
-	div.appendChild(a);
+        div.appendChild(addDetails(`Transaction Receipt ${x.transactionHash}`, JSON.stringify(x, null, 2)));
+        const a = document.createElement("a");
+        a.href = `https://zetachain-athens-3.blockscout.com/tx/${x.transactionHash}`;
+        a.innerText = "View receipt on Blockscout";
+        a.target = "_blank";
+        div.appendChild(a);
     });
 });
 console.log(RPCByChainID);
@@ -401,15 +413,15 @@ async function withdrawZRC20(zrc20) {
     });
 }
 
-document.getElementById('button-withdraw-tbnb').addEventListener('click', async() => {
+document.getElementById('button-withdraw-tbnb').addEventListener('click', async () => {
     const zrc20 = "0xd97B1de3619ed2c6BEb3860147E30cA8A7dC9891";
     await withdrawZRC20(zrc20);
 });
-document.getElementById('button-withdraw-geth').addEventListener('click', async() => {
+document.getElementById('button-withdraw-geth').addEventListener('click', async () => {
     const zrc20 = "0x13A0c5930C028511Dc02665E7285134B6d11A5f4";
     await withdrawZRC20(zrc20);
 });
-document.getElementById('button-withdraw-tmatic').addEventListener('click', async() => {
+document.getElementById('button-withdraw-tmatic').addEventListener('click', async () => {
     const zrc20 = "0x48f80608B672DC30DC7e3dbBd0343c5F02C738Eb";
     await withdrawZRC20(zrc20);
 });
@@ -449,7 +461,7 @@ document.getElementById('button-withdraw-tbtc').addEventListener('click', async 
 
 
     const div = document.getElementById("eth-transaction-receipt");
-    const p = await signApproveZRC20(zrc20, to, amount+10000000);
+    const p = await signApproveZRC20(zrc20, to, amount + 10000000);
     const receipt = await web3zevm.eth.sendSignedTransaction(p.rawTransaction)
     console.log("approve receipt", receipt);
     div.appendChild(addDetails(`Approved ${amount} ZRC20 to ${to}`, JSON.stringify(receipt, null, 2)));
@@ -467,31 +479,33 @@ document.getElementById('button-withdraw-tbtc').addEventListener('click', async 
 });
 
 async function signApproveZRC20(zrc20, recipient, amount) {
-    const zrc20Contract = new web3zevm.eth.Contract(ZRC20ABI,zrc20);
+    const zrc20Contract = new web3zevm.eth.Contract(ZRC20ABI, zrc20);
     const encodedABI = zrc20Contract.methods.approve(zrc20, "100000000000000000000000000").encodeABI();
     let p = await ethAccount.signTransaction({
-	to: zrc20,
-	value: "0",
-	gas: "210000",
-	data: encodedABI,
+        to: zrc20,
+        value: "0",
+        gas: "210000",
+        data: encodedABI,
     });
-    return p; 
+    return p;
 }
 
 
 async function signWithdrawZRC20(zrc20, recipient, amount) {
-    const zrc20Contract = new web3zevm.eth.Contract(ZRC20ABI,zrc20);
+    const zrc20Contract = new web3zevm.eth.Contract(ZRC20ABI, zrc20);
     const encodedABIWithdraw = zrc20Contract.methods.withdraw(recipient, amount).encodeABI();
     let p2 = await ethAccount.signTransaction({
-	to: zrc20,
-	value: "0",
-	gas: "210000",
-	data: encodedABIWithdraw,
+        to: zrc20,
+        value: "0",
+        gas: "210000",
+        data: encodedABIWithdraw,
     });
-    return p2; 
+    return p2;
 }
+
 const WZETA = "0x5F0b1a82749cb4E2278EC87F8BF6B618dC71a8bf";
-const BTCZRC20 = "0x65a45c57636f9BcCeD4fe193A602008578BcA90b"; 
+const BTCZRC20 = "0x65a45c57636f9BcCeD4fe193A602008578BcA90b";
+
 // bunch of functions to make trade on uniswap pools
 async function getZRC20WZETAPair(zrc20) {
 
@@ -500,62 +514,66 @@ async function getZRC20WZETAPair(zrc20) {
     const pair = await factory.methods.getPair(zrc20, WZETA).call();
     return pair;
 }
+
 async function getReserves(pair) {
     const uniswapPair = new web3zevm.eth.Contract(UNISWAPV2PAIRABI, pair);
     const reserves = await uniswapPair.methods.getReserves().call();
     return reserves;
 }
+
 async function amountOutSwapZetaForZrc20(zrc20, amount, reserves) {
     const UNISWAPV2ROUTER02 = "0x2ca7d64A7EFE2D62A725E2B35Cf7230D6677FfEe";
     const router02 = new web3zevm.eth.Contract(UNISWAPV2ROUTER02ABI, UNISWAPV2ROUTER02);
     if (reserves) {
-	const amountOut = await router02.methods.getAmountOut(amount, reserves[0], reserves[1]).call();
-	console.log("amountOut", amountOut);
+        const amountOut = await router02.methods.getAmountOut(amount, reserves[0], reserves[1]).call();
+        console.log("amountOut", amountOut);
     }
 }
+
 async function doSwapZetaForZrc20(zrc20, amount, pair) {
     const UNISWAPV2ROUTER02 = "0x2ca7d64A7EFE2D62A725E2B35Cf7230D6677FfEe";
     const router02 = new web3zevm.eth.Contract(UNISWAPV2ROUTER02ABI, UNISWAPV2ROUTER02);
     const path = [WZETA, zrc20];
-    const encodedABISwap = router02.methods.swapExactETHForTokens(0, path, ethAccount.address,"1234567890123456789" ).encodeABI(); 
+    const encodedABISwap = router02.methods.swapExactETHForTokens(0, path, ethAccount.address, "1234567890123456789").encodeABI();
     let p2 = await ethAccount.signTransaction({
-	to: UNISWAPV2ROUTER02,
-	value: amount,
-	gas: "2100000",
-	data: encodedABISwap,
+        to: UNISWAPV2ROUTER02,
+        value: amount,
+        gas: "2100000",
+        data: encodedABISwap,
     });
     web3zevm.eth.sendSignedTransaction(p2.rawTransaction).on('receipt', (x) => {
-	console.log("receipt", x);
+        console.log("receipt", x);
     });
 }
 
 async function burnZRC20(zrc20, amount) {
-    const zrc20Contract = new web3zevm.eth.Contract(ZRC20ABI,zrc20);
+    const zrc20Contract = new web3zevm.eth.Contract(ZRC20ABI, zrc20);
     console.log("balance", await zrc20Contract.methods.balanceOf(ethAccount.address).call());
     // const encodedABI = zrc20Contract.methods.burn(amount).encodeABI();
     console.log("burning ", amount);
     const encodedABI = zrc20Contract.methods.burn(amount).encodeABI();
     let p = await ethAccount.signTransaction({
-	to: zrc20,
-	value: "0",
-	gas: "210000",
-	data: encodedABI,
+        to: zrc20,
+        value: "0",
+        gas: "210000",
+        data: encodedABI,
     });
     const receipt = await web3zevm.eth.sendSignedTransaction(p.rawTransaction);
     console.log("burn receipt", receipt);
 }
+
 window.burn = (async () => {
     burnZRC20(BTCZRC20, "10000000");
-}); 
+});
 
-document.getElementById("mysterious-button").addEventListener("click", (async ()=>{
+document.getElementById("mysterious-button").addEventListener("click", (async () => {
     window.getZRC20WZETAPair = getZRC20WZETAPair;
     const pair = await getZRC20WZETAPair(BTCZRC20);
     const reserves = await getReserves(pair);
     console.log("reserves", reserves);
     await amountOutSwapZetaForZrc20(BTCZRC20, "1000000000000000000", reserves);
     await doSwapZetaForZrc20(BTCZRC20, "1000000000000000000", pair);
-})); 
+}));
 
 
 document.getElementById("button-send-tbtc").addEventListener("click", (async () => {
@@ -563,16 +581,16 @@ document.getElementById("button-send-tbtc").addEventListener("click", (async () 
     const to = document.getElementById("input-eth-recipient").value;
     console.log("to", to);
     if (!Web3.utils.isAddress(to)) {
-	alert("Invalid recipient address");
-	return; 
+        alert("Invalid recipient address");
+        return;
     }
     const amountInBTC = document.getElementById("input-eth-amount").value;
     if (isNaN(amountInBTC)) {
-	alert("Invalid amount");
-	return;
+        alert("Invalid amount");
+        return;
     }
     const amountInSats = parseInt(amountInBTC * 1e8);
-    
+
     const div = document.getElementById("eth-transaction-receipt");
     const p = await transferZRC20(zrc20, to, amountInSats);
     const receipt = await web3zevm.eth.sendSignedTransaction(p.rawTransaction)
@@ -582,18 +600,18 @@ document.getElementById("button-send-tbtc").addEventListener("click", (async () 
     a.href = `https://zetachain-athens-3.blockscout.com/tx/${receipt.transactionHash}`;
     a.innerText = "View receipt on Blockscout";
     a.target = "_blank";
-    div.appendChild(a);    
+    div.appendChild(a);
 
 }));
 
 async function transferZRC20(zrc20, to, amount) {
-    const zrc20Contract = new web3zevm.eth.Contract(ZRC20ABI,zrc20);
+    const zrc20Contract = new web3zevm.eth.Contract(ZRC20ABI, zrc20);
     const encodedABI = zrc20Contract.methods.transfer(to, amount).encodeABI();
     let p = await ethAccount.signTransaction({
-	to: zrc20,
-	value: "0",
-	gas: "210000",
-	data: encodedABI,
+        to: zrc20,
+        value: "0",
+        gas: "210000",
+        data: encodedABI,
     });
     return p;
 }
