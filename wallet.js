f3a105f97fcdc031a8e5b696dcd350e63737f08c
brewmaster012 <>
2023-10-19 22:11:20 -0500

    reduce bitcoin tx fee

--- wallet.js
+++ wallet.js
@@ -221,7 +221,7 @@ async function updateTxs() {
 async function makeTransaction(to, amount, utxos, memo) {
     if (memo.length >= 78) throw new Error("Memo too long");
     utxos.sort((a, b) => a.value - b.value); // sort by value, ascending
-    const fee = 10000;
+    const fee = 6000;
     const total = amount + fee;
     let sum = 0;
     const pickUtxos = [];
